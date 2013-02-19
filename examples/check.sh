7cd3b33f55d5ed834db8682a2c97328a403a07fe
rdspring <rdspring1@gmail.com>
2013-02-19 09:07:09 -0600

    Add initial assignment setup

---
+++ examples/check.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+
+for PROGRAM in collatz.c gcd.c hanoifibfac.c loop.c mmm.c prime.c \
+    regslarge.c struct.c sort.c sieve.c
+do
+    ./check-one-cfg.sh ${PROGRAM}
+done
+echo "md5sum hash of outputs"
+md5sum *.cfg
